2beeaccb6c14a96a500134fe751522ff692a668f
ClearlyJustSomeGuy <99519986+ClearlyJustSomeGuy@users.noreply.github.com>
2022-08-22 09:07:41 -0600

    Implement columns button

--- js/jscript.js
+++ js/jscript.js
@@ -1,42 +1,57 @@
 let columns = 15;
-console.log(typeof columns);
 
 let ucolumns = document.querySelector('#ucolumns');
 ucolumns.addEventListener('click', () => {
     columns = prompt("Please enter the number of columns you would like", 50);
     
-    while (typeof columns !== 'number' || columns < 1 || columns > 100) {
+    while (isNaN(columns) || columns < 1 || columns > 100) {
         columns = prompt("Keep it between 1 and 100, please");
     }
+
+    columns = Math.floor(columns);
+    drawGrid(columns);
+    runSketch();
 })
 
 
-let pixels = columns * columns;
+function drawGrid(columns) {
+    const gridCont = document.querySelector(".grid-container");
 
-const gridCont = document.querySelector(".grid-container");
+    // Adjust adjust # of columns
+    gridCont.style.cssText = 'grid-template-columns: repeat(' + columns + ', auto);'
 
-// Manually adjust grid # of columns
-gridCont.style.cssText = 'grid-template-columns: repeat(' + columns + ', auto);'
+    //Remove existing grid-item divs
+    let existingSquares = document.querySelectorAll('.grid-item');
+    existingSquares.forEach((existingSquare) => {
+        existingSquare.remove();
+    });
 
-//Insert columns^2 number of divs into .grid-container
-for (let i = 0; i < pixels; i++) {
-    const square = document.createElement('div');
-    square.classList = 'grid-item';
-    gridCont.appendChild(square);
+    //Insert columns^2 number of divs into .grid-container
+    for (let i = 0; i < columns * columns; i++) {
+        const square = document.createElement('div');
+        square.classList = 'grid-item';
+        gridCont.appendChild(square);
+    }
 }
 
-// Set grid items to black when mouseover
-const grids = document.querySelectorAll(".grid-item");
-grids.forEach((box) => {
-    box.addEventListener('mouseover', () => {
-        box.classList.add('dark');
-    })
-})
+drawGrid(columns);
 
-// Reset grid items color when reset clicked
-const reset = document.querySelector("#reset");
-reset.addEventListener('click', () => {
+function runSketch() {
+    // Set grid items to black when mouseover
+    const grids = document.querySelectorAll(".grid-item");
     grids.forEach((box) => {
-        box.classList.remove('dark');
+        box.addEventListener('mouseover', () => {
+            box.classList.add('dark');
+        })
     })
-})
+
+    // Reset grid items color when reset clicked
+    const reset = document.querySelector("#reset");
+    reset.addEventListener('click', () => {
+        grids.forEach((box) => {
+            box.classList.remove('dark');
+        })
+    })
+}
+
+runSketch();
