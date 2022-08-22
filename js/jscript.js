96fcadecabe535d49923dcaba2dd2498c3a2e834
ClearlyJustSomeGuy <99519986+ClearlyJustSomeGuy@users.noreply.github.com>
2022-08-22 14:50:47 -0600

    break out functions, start random color

--- js/jscript.js
+++ js/jscript.js
@@ -1,30 +1,27 @@
 let columns = 15;
 
-let ucolumns = document.querySelector('#ucolumns');
-ucolumns.addEventListener('click', () => {
-    columns = prompt("Please enter the number of columns you would like", 50);
-    
-    while (isNaN(columns) || columns < 1 || columns > 100) {
-        columns = prompt("Keep it between 1 and 100, please");
-    }
-
-    columns = Math.floor(columns);
-    drawGrid(columns);
-    runSketch();
-})
+// //Check for change in colors checkbox
+// let colorCheckbox = document.querySelector('#colors');
+// colorCheckbox.addEventListener('change', () => {
+//     if (colorCheckbox.checked) {
+//         drawColor();
+//     }
+//     drawGray();
+// })
 
 
+
+// Draw the grid layout
 function drawGrid(columns) {
-    const gridCont = document.querySelector(".grid-container");
-
-    // Adjust adjust # of columns
-    gridCont.style.cssText = 'grid-template-columns: repeat(' + columns + ', auto);'
-
     //Remove existing grid-item divs
     let existingSquares = document.querySelectorAll('.grid-item');
     existingSquares.forEach((existingSquare) => {
         existingSquare.remove();
     });
+    const gridCont = document.querySelector(".grid-container");
+
+    // Adjust adjust # of columns
+    gridCont.style.cssText = 'grid-template-columns: repeat(' + columns + ', auto);'
 
     //Insert columns^2 number of divs into .grid-container
     for (let i = 0; i < columns * columns; i++) {
@@ -36,22 +33,70 @@ function drawGrid(columns) {
 
 drawGrid(columns);
 
-function runSketch() {
-    // Set grid items to black when mouseover
+
+
+function drawGray() {
+    // Set grid items to black when mouseover   
     const grids = document.querySelectorAll(".grid-item");
     grids.forEach((box) => {
         box.addEventListener('mouseover', () => {
-            box.classList.add('dark');
-        })
-    })
-
-    // Reset grid items color when reset clicked
-    const reset = document.querySelector("#reset");
-    reset.addEventListener('click', () => {
-        grids.forEach((box) => {
-            box.classList.remove('dark');
+            box.style.background = 'black';
+            let itemOpacity = box.style.opacity;
+            if (itemOpacity) {
+                itemOpacity = parseFloat(itemOpacity);
+                itemOpacity += 0.2;
+                box.style.opacity = itemOpacity;
+            } else {
+                box.style.opacity = 0.2;
+            }
+            
         })
     })
 }
 
-runSketch();
+
+// function drawColor() {
+//     // Set grid items to black when mouseover   
+//     grids.forEach((box) => {
+//         box.addEventListener('mouseover', () => {
+//             // let color = getRandomColor();
+//             box.style.background = getRandomColor();           
+//         })
+//     })
+// }
+
+
+function resetGrid() {
+    const grids = document.querySelectorAll(".grid-item");
+    grids.forEach((box) => {
+        // box.classList.remove('dark');
+        box.removeAttribute('style');
+    })
+}
+
+function getRandomColor() {
+    let r = Math.random() * 255;
+    let g = Math.random() * 255;
+    let b = Math.random() * 255;
+    return `rgb(${r}, ${g}, ${b})`
+}
+
+drawGray();
+
+// Reset grid items color when reset clicked
+const reset = document.querySelector("#reset");
+reset.addEventListener('click', resetGrid);
+
+//Prompt user for column width, update html and restart sketch function
+let ucolumns = document.querySelector('#ucolumns');
+ucolumns.addEventListener('click', () => {
+    columns = prompt("Please enter the number of columns you would like", 50);
+    
+    while (isNaN(columns) || columns < 1 || columns > 100) {
+        columns = prompt("Keep it between 1 and 100, please");
+    }
+
+    columns = Math.floor(columns);
+    drawGrid(columns);
+    drawGray();
+})
