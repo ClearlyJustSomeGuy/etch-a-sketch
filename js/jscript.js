92b6c920df0f4f5244d63a2abf2dc9eb9dc89363
blaine_top <blainedh@gmail.com>
2022-08-22 07:52:41 -0600

    Add prompt for columns

--- js/jscript.js
+++ js/jscript.js
@@ -1,4 +1,29 @@
-let columns = 10;
+let columns = 15;
+console.log(typeof columns);
+
+let ucolumns = document.querySelector('#ucolumns');
+ucolumns.addEventListener('click', () => {
+    columns = prompt("Please enter the number of columns you would like", 50);
+    
+    while (typeof columns !== 'number' || columns < 1 || columns > 100) {
+        columns = prompt("Keep it between 1 and 100, please");
+    }
+})
+
+
+let pixels = columns * columns;
+
+const gridCont = document.querySelector(".grid-container");
+
+// Manually adjust grid # of columns
+gridCont.style.cssText = 'grid-template-columns: repeat(' + columns + ', auto);'
+
+//Insert columns^2 number of divs into .grid-container
+for (let i = 0; i < pixels; i++) {
+    const square = document.createElement('div');
+    square.classList = 'grid-item';
+    gridCont.appendChild(square);
+}
 
 // Set grid items to black when mouseover
 const grids = document.querySelectorAll(".grid-item");
@@ -15,9 +40,3 @@ reset.addEventListener('click', () => {
         box.classList.remove('dark');
     })
 })
-
-// Manually adjust grid # of columns
-const gridCont = document.querySelector(".grid-container");
-gridCont.style.cssText = 'grid-template-columns: repeat(' + columns + ', auto);';
-
-//Insert columns^2 number of divs into .grid-container
